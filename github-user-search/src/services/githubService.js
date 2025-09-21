43a4b35608dbbc6e94f34d9d991a5a15f6299dfc
mabuzaS21 <mabuzasfiso05@gmail.com>
2025-09-21 14:23:01 +0200

    Deploy GitHub User Search App to Vercel

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -13,15 +13,14 @@ export const fetchUserData = async (username, location, minRepos) => {
       query += `+repos:>=${minRepos}`;  
     }
 
-    console.log("Constructed query:", query);
-
     const apiUrl = `${GITHUB_API_URL}${query}`;
-    console.log("Final API URL:", apiUrl);  
-
+    
     const response = await axios.get(apiUrl);
     return response.data.items;  
   } catch (error) {
+    
     console.error("Error fetching user data:", error.response ? error.response.data : error);
-    return null;  
+
+    throw new Error("Failed to fetch user data. Please try again.");
   }
 };
