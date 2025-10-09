7912f1641fbc387b91827dfb557a2a7685d40680
mabuzaS21 <mabuzasfiso05@gmail.com>
2025-10-09 12:17:25 +0200

    Implement React Query fetching of posts with loading, error, and refetch

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return res.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+
+  if (isLoading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}><strong>{post.title}</strong><p>{post.body}</p></li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
