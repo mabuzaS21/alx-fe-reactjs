2fc082b9225199a0db681bf74166f02924f37d10
mabuzaS21 <mabuzasfiso05@gmail.com>
2025-10-09 12:20:56 +0200

    Add React Query setup with QueryClientProvider in App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import PostsComponent from './components/PostsComponent.jsx';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
